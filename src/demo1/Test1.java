3d674a5a79cac85a4b696c0240eccc824f24e0aa
manegauri18 <132036623+manegauri18@users.noreply.github.com>
2025-07-17 23:23:51 +0530

    Update Test1.java

--- src/demo1/Test1.java
+++ src/demo1/Test1.java
@@ -3,8 +3,10 @@ package demo1;
 public class Test1 {
 
 	public static void main(String[] args) {
+
+               System.out.println("GOOD Morning");
 		
-		System.out.println("GOOD NIGHT");
+		System.out.println("Have a nice day");
 		
 	}
 	
